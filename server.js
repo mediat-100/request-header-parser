de5caa101da9625a1de97733adf784649f695ccb
Mediat Yusuff <yusuftomiwa740@gmail.com>
2021-11-08 11:45:57 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const Window = require('window');
+const dotenv = require('dotenv');
+const ip = require('ip');
+
+const express = require('express');
+
+dotenv.config({ path: './config.env' });
+
+const window = new Window();
+const app = express();
+
+app.get('/api/whoami', (req, res) => {
+  let myIpAddress = ip.address();
+  let myLanguage = window.navigator.language;
+  let mySoftware = window.navigator.userAgent;
+  res.json({
+    ipaddress: myIpAddress,
+    language: myLanguage,
+    software: mySoftware,
+  });
+});
+
+// Listener
+app.listen(process.env.PORT, '127.0.0.1', () => {
+  console.log('app running on port', process.env.PORT);
+});
